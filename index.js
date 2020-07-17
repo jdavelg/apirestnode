78001c2d2aff3eec65820d159fbafec6eb714bee
jonathan lopez <jdavelg@gmail.com>
2020-07-16 21:34:00 -0600

    added nodemon to dependencies

--- index.js
+++ index.js
@@ -8,15 +8,15 @@ var port= 3700;
 mongoose.Promise=global.Promise;
 mongoose.set('useNewUrlParser', true );
 mongoose.set('useUnifiedTopology', true);
-mongoose.connect('mongodb://localhost:27017/portafolio')
+mongoose.connect(process.env.MONGODB_URI ||'mongodb://localhost:27017/portafolio')
     .then(()=>{
 
         console.log("connection succesfully");
         
         //Creacion del servidor
 
-app.listen(port, 'localhost',()=>{
-    console.log("server running succesfully on localhost:3700");
+        app.listen(process.env.PORT || 8080, function () {
+            var port = server.address().port;
     
 })
     })
